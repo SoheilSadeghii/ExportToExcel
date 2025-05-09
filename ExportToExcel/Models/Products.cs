004e2328d5781e8ff0355928e0434bf12c73b475
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2025-05-09 16:14:02 +0330

    Created the Product model class.

---
+++ ExportToExcel/Models/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExportToExcel
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
